b7cff762783a020fb1bbd789f07fcb4fbdb6a3bc
Julien Lamy <lamy@unistra.fr>
2015-03-12 16:06:50 +0100

    Add Association

---
+++ src/dcmtkpp/Association.cpp
@@ -0,0 +1,436 @@
+/*************************************************************************
+ * dcmtkpp - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#include "Association.h"
+
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <dcmtk/config/osconfig.h>
+#include <dcmtk/dcmnet/assoc.h>
+#include <dcmtk/dcmnet/cond.h>
+
+#include "dcmtkpp/Exception.h"
+
+namespace dcmtkpp
+{
+
+Association
+::Association()
+: _own_ae_title(""),
+  _peer_host_name(""), _peer_port(104), _peer_ae_title(""),
+  _user_identity_type(UserIdentityType::None), 
+  _user_identity_primary_field(""), _user_identity_secondary_field(""),
+  _association(NULL)
+{
+    // Nothing else
+}
+
+Association
+::Association(Association const & other)
+: _own_ae_title(other.get_own_ae_title()), 
+  _peer_host_name(other.get_peer_host_name()), _peer_port(other.get_peer_port()),
+  _peer_ae_title(other.get_peer_ae_title()),
+  _user_identity_type(other.get_user_identity_type()), 
+  _user_identity_primary_field(other.get_user_identity_primary_field()), 
+  _user_identity_secondary_field(other.get_user_identity_secondary_field())
+{
+}
+
+Association
+::~Association()
+{
+    if(this->is_associated())
+    {
+        this->release();
+    }
+}
+
+Association &
+Association
+::operator=(Association const & other)
+{
+    if(this != &other)
+    {
+        this->set_own_ae_title(other.get_own_ae_title());
+        this->set_peer_host_name(other.get_peer_host_name());
+        this->set_peer_port(other.get_peer_port());
+        this->set_peer_ae_title(other.get_peer_ae_title());
+        this->set_user_identity_type(other.get_user_identity_type());
+        this->set_user_identity_primary_field(other.get_user_identity_primary_field());
+        this->set_user_identity_secondary_field(other.get_user_identity_secondary_field());
+    }
+    
+    return *this;
+}
+
+std::string const &
+Association
+::get_own_ae_title() const
+{
+    return this->_own_ae_title;
+}
+
+void
+Association
+::set_own_ae_title(std::string const & ae_title)
+{
+    if(this->is_associated())
+    {
+        throw Exception("Cannot set member while associated");
+    }
+    
+    this->_own_ae_title = ae_title;
+}
+
+std::string const &
+Association
+::get_peer_host_name() const
+{
+    return this->_peer_host_name;
+}
+
+void
+Association
+::set_peer_host_name(std::string const & host_name)
+{
+    if(this->is_associated())
+    {
+        throw Exception("Cannot set member while associated");
+    }
+    
+    this->_peer_host_name = host_name;
+}
+
+uint16_t
+Association
+::get_peer_port() const
+{
+    return this->_peer_port;
+}
+
+void
+Association
+::set_peer_port(uint16_t port)
+{
+    if(this->is_associated())
+    {
+        throw Exception("Cannot set member while associated");
+    }
+    
+    this->_peer_port = port;
+}
+
+std::string const &
+Association
+::get_peer_ae_title() const
+{
+    return this->_peer_ae_title;
+}
+
+void
+Association
+::set_peer_ae_title(std::string const & ae_title)
+{
+    if(this->is_associated())
+    {
+        throw Exception("Cannot set member while associated");
+    }
+    
+    this->_peer_ae_title = ae_title;
+}
+
+void
+Association
+::add_presentation_context(std::string const & abstract_syntax,
+    std::vector<std::string> const & transfer_syntaxes)
+{
+    if(this->is_associated())
+    {
+        throw Exception("Cannot set member while associated");
+    }
+    
+    this->_presentation_contexts.push_back(
+        std::make_pair(abstract_syntax, transfer_syntaxes));
+}
+
+UserIdentityType
+Association
+::get_user_identity_type() const
+{
+    return this->_user_identity_type;
+}
+
+void
+Association
+::set_user_identity_type(UserIdentityType type)
+{
+    if(this->is_associated())
+    {
+        throw Exception("Cannot set member while associated");
+    }
+    
+    this->_user_identity_type = type;
+}
+
+std::string const &
+Association
+::get_user_identity_primary_field() const
+{
+    return this->_user_identity_primary_field;
+}
+
+void
+Association
+::set_user_identity_primary_field(std::string const & value)
+{
+    if(this->is_associated())
+    {
+        throw Exception("Cannot set member while associated");
+    }
+    
+    this->_user_identity_primary_field = value;
+}
+
+std::string const &
+Association
+::get_user_identity_secondary_field() const
+{
+    return this->_user_identity_secondary_field;
+}
+
+void
+Association
+::set_user_identity_secondary_field(std::string const & value)
+{
+    if(this->is_associated())
+    {
+        throw Exception("Cannot set member while associated");
+    }
+    
+    this->_user_identity_secondary_field = value;
+}
+
+void
+Association
+::set_user_identity_to_none()
+{
+    this->set_user_identity_type(UserIdentityType::None);
+    this->set_user_identity_primary_field("");
+    this->set_user_identity_secondary_field("");
+}
+
+void
+Association
+::set_user_identity_to_username(std::string const & username)
+{
+    this->set_user_identity_type(UserIdentityType::Username);
+    this->set_user_identity_primary_field(username);
+    this->set_user_identity_secondary_field("");
+}
+
+void
+Association
+::set_user_identity_to_username_and_password(
+    std::string const & username, std::string const & password)
+{
+    this->set_user_identity_type(UserIdentityType::UsernameAndPassword);
+    this->set_user_identity_primary_field(username);
+    this->set_user_identity_secondary_field(password);
+}
+    
+void
+Association
+::set_user_identity_to_kerberos(std::string const & ticket)
+{
+    this->set_user_identity_type(UserIdentityType::Kerberos);
+    this->set_user_identity_primary_field(ticket);
+    this->set_user_identity_secondary_field("");
+}
+
+void
+Association
+::set_user_identity_to_saml(std::string const & assertion)
+{
+    this->set_user_identity_type(UserIdentityType::SAML);
+    this->set_user_identity_primary_field(assertion);
+    this->set_user_identity_secondary_field("");
+}
+
+bool
+Association
+::is_associated() const
+{
+    return (this->_association != NULL);
+}
+
+void
+Association
+::associate(Network & network)
+{
+    if(!network.is_initialized())
+    {
+        throw Exception("Network is not initialized");
+    }
+    
+    if(this->is_associated())
+    {
+        throw Exception("Already associated");
+    }
+    
+    OFCondition condition;
+    
+    T_ASC_Parameters * params;
+    condition = ASC_createAssociationParameters(&params, ASC_MAXIMUMPDUSIZE);
+    if(condition.bad())
+    {
+        throw Exception(condition);
+    }
+    
+    condition = ASC_setAPTitles(params, 
+        this->_own_ae_title.c_str(), this->_peer_ae_title.c_str(), NULL);
+    if(condition.bad())
+    {
+        ASC_destroyAssociationParameters(&params);
+        throw Exception(condition);
+    }
+    
+    std::string localhost(128, '\0');
+    gethostname(&localhost[0], localhost.size()-1);
+    
+    std::ostringstream peer;
+    peer << this->_peer_host_name << ":" << this->_peer_port;
+    
+    condition = ASC_setPresentationAddresses(params,
+        "localhost", peer.str().c_str());
+    if(condition.bad())
+    {
+        ASC_destroyAssociationParameters(&params);
+        throw Exception(condition);
+    }
+    
+    unsigned int context_id = 1;
+    for(auto const & context: this->_presentation_contexts)
+    {
+        char const ** transfer_syntaxes = new char const *[context.second.size()];
+        for(std::size_t i = 0; i < context.second.size(); ++i)
+        {
+            transfer_syntaxes[i] = context.second[i].c_str();
+        }
+        
+        condition = ASC_addPresentationContext(params, 
+            context_id, context.first.c_str(), 
+            transfer_syntaxes, context.second.size());
+        if(condition.bad())
+        {
+            ASC_destroyAssociationParameters(&params);
+            throw Exception(condition);
+        }
+        
+        context_id += 2;
+    }
+    
+    if(this->_user_identity_type == UserIdentityType::None)
+    {
+        // Nothing to do.
+    }
+    else if(this->_user_identity_type == UserIdentityType::Username)
+    {
+        condition = ASC_setIdentRQUserOnly(params, 
+            this->_user_identity_primary_field.c_str());
+    }
+    else if(this->_user_identity_type == UserIdentityType::UsernameAndPassword)
+    {
+        condition = ASC_setIdentRQUserOnly(params, 
+            this->_user_identity_primary_field.c_str(),
+            this->_user_identity_secondary_field.c_str());
+    }
+    else if(this->_user_identity_type == UserIdentityType::Kerberos)
+    {
+        condition = ASC_setIdentRQKerberos(params, 
+            this->_user_identity_primary_field.c_str(),
+            this->_user_identity_primary_field.size());
+    }
+    else if(this->_user_identity_type == UserIdentityType::SAML)
+    {
+        condition = ASC_setIdentRQSaml(params, 
+            this->_user_identity_primary_field.c_str(),
+            this->_user_identity_primary_field.size());
+    }
+    else
+    {
+        ASC_destroyAssociationParameters(&params);
+        throw Exception("Unknown identity type");
+    }
+    
+    if(condition.bad())
+    {
+        ASC_destroyAssociationParameters(&params);
+        throw Exception(condition);
+    }
+    
+    condition = ASC_requestAssociation(
+        network.get_network(), params, &this->_association);
+    if(condition.bad())
+    {
+        OFString empty;
+        
+        if(condition == DUL_ASSOCIATIONREJECTED)
+        {
+            T_ASC_RejectParameters rej;
+            ASC_getRejectParameters(params, &rej);
+            
+            ASC_destroyAssociationParameters(&params);
+            throw Exception(ASC_printRejectParameters(empty, &rej).c_str());
+        } 
+        else 
+        {
+            ASC_destroyAssociationParameters(&params);
+            throw Exception(DimseCondition::dump(empty, condition).c_str());
+        }
+    }
+}
+
+T_ASC_Association *
+Association
+::get_association()
+{
+    return this->_association;
+}
+
+void
+Association
+::release()
+{
+    if(!this->is_associated())
+    {
+        throw Exception("Not associated");
+    }
+    
+    ASC_releaseAssociation(this->_association);
+    ASC_destroyAssociation(&this->_association);
+    this->_association = NULL;
+}
+
+void
+Association
+::abort()
+{
+    if(!this->is_associated())
+    {
+        throw Exception("Not associated");
+    }
+    
+    ASC_abortAssociation(this->_association);
+    ASC_destroyAssociation(&this->_association);
+    this->_association = NULL;
+}
+
+}
---
+++ src/dcmtkpp/Association.h
@@ -0,0 +1,161 @@
+/*************************************************************************
+ * dcmtkpp - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#ifndef _a52696bc_5c6e_402d_a343_6cb085eb0138
+#define _a52696bc_5c6e_402d_a343_6cb085eb0138
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <dcmtk/config/osconfig.h>
+#include <dcmtk/dcmnet/assoc.h>
+
+#include "dcmtkpp/Network.h"
+
+namespace dcmtkpp
+{
+
+/// @brief User identity types as defined by PS3.8 2013 D.3.3.7
+enum class UserIdentityType
+{
+    None = 0,
+    Username = 1,
+    UsernameAndPassword = 2,
+    Kerberos = 3,
+    SAML = 4
+};
+
+/**
+ * @brief Wrapper around the T_ASC_Association class.
+ * 
+ * No member can be set while the object is associated.
+ */
+class Association
+{
+public:
+    /// @brief Create a default, un-associated, association.
+    Association();
+    
+    /// @brief Create an un-associated association.
+    Association(Association const & other);
+    
+    /// @brief Destroy the association, release it if necessary.
+    ~Association();
+    
+    /// @brief Assing an un-associated association; it remains un-associated.
+    Association & operator=(Association const & other);
+    
+    /// Return the AE title of the caller. Defaults to "".
+    std::string const & get_own_ae_title() const;
+    /// Set the AE title of the caller.
+    void set_own_ae_title(std::string const & ae_title);
+    
+    /// @name Peer
+    /// @{
+    
+    /// Return the host name of the peer. Defaults to "".
+    std::string const & get_peer_host_name() const;
+    /// Set the host name of the peer.
+    void set_peer_host_name(std::string const & host_name);
+    
+    /// Return the port of the peer. Defaults to 104.
+    uint16_t get_peer_port() const;
+    /// Set the port of the peer.
+    void set_peer_port(uint16_t port);
+    
+    /// Return the AE title of the peer. Defaults to "".
+    std::string const & get_peer_ae_title() const;
+    /// Set the AE title of the peer.
+    void set_peer_ae_title(std::string const & ae_title);
+    
+    /// @}
+    
+    /// @name Presentation contexts
+    /// @{
+    
+    void add_presentation_context(std::string const & abstract_syntax,
+        std::vector<std::string> const & transfer_syntaxes);
+    
+    /// @}
+    
+    /// @name User identity
+    /// @{
+        
+    /// Return the user identity type. Defaults to None.
+    UserIdentityType get_user_identity_type() const;
+    /// Set the user identity type.
+    void set_user_identity_type(UserIdentityType type);
+    
+    /// Return the user identity primary field. Defaults to "".
+    std::string const & get_user_identity_primary_field() const;
+    /// Set the user identity primary field. 
+    void set_user_identity_primary_field(std::string const & value);
+    
+    /// Return the user identity secondary field. Defaults to "".
+    std::string const & get_user_identity_secondary_field() const;
+    /// Set the user identity secondary field.
+    void set_user_identity_secondary_field(std::string const & value);
+    
+    /// Do no authenticate user.
+    void set_user_identity_to_none();
+    
+    /// Authenticate user using only a username.
+    void set_user_identity_to_username(std::string const & username);
+    
+    /// Authenticate user using a username and a password.
+    void set_user_identity_to_username_and_password(
+        std::string const & username, std::string const & password);
+        
+    /// Authenticate user using a Kerberos ticket.
+    void set_user_identity_to_kerberos(std::string const & ticket);
+    
+    /// Authenticate user using a SAML assertion.
+    void set_user_identity_to_saml(std::string const & assertion);
+    
+    /// @}
+    
+    /// @name Association
+    /// @{
+    
+    /// Test whether the object is currently associated to its peer.
+    bool is_associated() const;
+    
+    /// Associate with the peer. Throws an exception if already associated.
+    void associate(Network & network);
+    
+    /// Return the association object.
+    T_ASC_Association * get_association();
+    
+    /// Gracefully release the association. Throws an exception if not associated.
+    void release();
+    /// Forcefully release the association. Throws an exception if not associated.
+    void abort();
+    
+    /// @}
+
+private:
+    std::string _own_ae_title;
+    
+    std::string _peer_host_name;
+    uint16_t _peer_port;
+    std::string _peer_ae_title;
+    
+    typedef std::pair<std::string, std::vector<std::string> > PresentationContext;
+    std::vector<PresentationContext> _presentation_contexts;
+    
+    UserIdentityType _user_identity_type;
+    std::string _user_identity_primary_field;
+    std::string _user_identity_secondary_field;
+    
+    T_ASC_Association * _association;
+};
+
+}
+
+#endif // _a52696bc_5c6e_402d_a343_6cb085eb0138
