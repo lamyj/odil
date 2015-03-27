a63d9b32f096a3a395a35a1c6ae81497c77a9867
Julien Lamy <lamy@unistra.fr>
2015-03-27 16:52:54 +0100

    Add Cancellation message

---
+++ src/dcmtkpp/Cancellation.cpp
@@ -0,0 +1,52 @@
+/*************************************************************************
+ * dcmtkpp - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#include "dcmtkpp/Cancellation.h"
+
+#include <dcmtk/config/osconfig.h>
+#include <dcmtk/dcmdata/dcdeftag.h>
+#include <dcmtk/dcmdata/dcvr.h>
+#include <dcmtk/dcmnet/dimse.h>
+#include <dcmtk/ofstd/oftypes.h>
+
+#include "dcmtkpp/Message.h"
+
+namespace dcmtkpp
+{
+
+Cancellation
+::Cancellation(Uint16 message_id_being_responded_to)
+: Message()
+{
+    this->set_command_field(DIMSE_C_CANCEL_RQ);
+    this->set_message_id_being_responded_to(message_id_being_responded_to);
+}
+
+Cancellation
+::Cancellation(Message const & message)
+{
+    if(message.get_command_field() != DIMSE_C_CANCEL_RQ)
+    {
+        throw Exception("Message is not a C-CANCEL-RQ");
+    }
+    this->set_command_field(message.get_command_field());
+
+    Uint16 const message_id = ElementAccessor<EVR_US>::get(
+        message.get_command_set(), DCM_MessageIDBeingRespondedTo);
+    this->set_message_id_being_responded_to(message_id);
+
+    this->set_data_set(message.get_data_set());
+}
+
+Cancellation
+::~Cancellation()
+{
+    // Nothing to do.
+}
+
+}
---
+++ src/dcmtkpp/Cancellation.h
@@ -0,0 +1,45 @@
+/*************************************************************************
+ * dcmtkpp - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#ifndef _97fc1bfc_4cff_40f2_a1ed_4550c71a0bda
+#define _97fc1bfc_4cff_40f2_a1ed_4550c71a0bda
+
+#include <dcmtk/config/osconfig.h>
+#include <dcmtk/dcmdata/dcdeftag.h>
+#include <dcmtk/dcmdata/dcvr.h>
+#include <dcmtk/ofstd/oftypes.h>
+
+#include "dcmtkpp/Message.h"
+
+namespace dcmtkpp
+{
+
+/// @brief Base class for cancellation messages.
+class Cancellation: public Message
+{
+public:
+    /// @brief Create a response with given message id being responded to.
+    Cancellation(Uint16 message_id_being_responded_to);
+
+    /**
+     * @brief Create a response from the Message ID Being Responded To.
+     *
+     * Raise an exception is this element is missing.
+     */
+    Cancellation(Message const & message);
+
+    /// @brief Destructor.
+    ~Cancellation();
+
+    DCMTKPP_MESSAGE_MANDATORY_FIELD_MACRO(
+        message_id_being_responded_to, DCM_MessageIDBeingRespondedTo, EVR_US);
+};
+
+}
+
+#endif // _97fc1bfc_4cff_40f2_a1ed_4550c71a0bda
