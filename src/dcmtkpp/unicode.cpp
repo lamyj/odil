5aa132fbe5aecde1076c8df4075c91a5a396d8fa
Julien Lamy <lamy@unistra.fr>
2015-05-22 17:34:20 +0200

    Add UTF-8 converter

---
+++ src/dcmtkpp/unicode.cpp
@@ -0,0 +1,331 @@
+/*************************************************************************
+ * dcmtkpp - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#include "dcmtkpp/unicode.h"
+
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+
+#include <unicode/errorcode.h>
+#include <unicode/unistr.h>
+
+#include "dcmtkpp/Exception.h"
+#include "dcmtkpp/Value.h"
+
+namespace dcmtkpp
+{
+
+std::map<std::string, std::string> const icu_encodings = {
+    { "", "" },
+    // Single-Byte Character Sets Without Code Extensions
+    { "ISO_IR 100", "iso-ir-100" },
+    { "ISO_IR 101", "iso-ir-101" },
+    { "ISO_IR 109", "iso-ir-109" },
+    { "ISO_IR 110", "iso-ir-110" },
+    { "ISO_IR 144", "iso-ir-144" },
+    { "ISO_IR 127", "iso-ir-127" },
+    { "ISO_IR 126", "iso-ir-126" },
+    { "ISO_IR 138", "iso-ir-138" },
+    { "ISO_IR 148", "iso-ir-148" },
+    { "ISO_IR 13", "Shift_JIS" }, // Since the 0x00 to 0x7f characters
+    // match ASCII (i.e. Romaji) and the 0xa1 to 0xdf map to half-width
+    // katakana of JIS X 0201. cf. https://en.wikipedia.org/wiki/Shift_JIS
+    { "ISO_IR 166", "TIS-620" },
+
+    // Single-Byte Character Sets with Code Extensions
+    { "ISO 2022 IR 6", "" },
+    { "ISO 2022 IR 100", "iso-ir-100" },
+    { "ISO 2022 IR 101", "iso-ir-101" },
+    { "ISO 2022 IR 109", "iso-ir-109" },
+    { "ISO 2022 IR 110", "iso-ir-110" },
+    { "ISO 2022 IR 144", "iso-ir-144" },
+    { "ISO 2022 IR 127", "iso-ir-127" },
+    { "ISO 2022 IR 126", "iso-ir-126" },
+    { "ISO 2022 IR 138", "iso-ir-138" },
+    { "ISO 2022 IR 148", "iso-ir-148" },
+    { "ISO 2022 IR 13", "Shift_JIS" },
+    { "ISO 2022 IR 166", "TIS-620" },
+
+    { "\x1B\x28\x42", "" },
+    { "\x1B\x2D\x41", "iso-ir-100" },
+    { "\x1B\x2D\x42", "iso-ir-101" },
+    { "\x1B\x2D\x43", "iso-ir-109" },
+    { "\x1B\x2D\x44", "iso-ir-110" },
+    { "\x1B\x2D\x4C", "iso-ir-144" },
+    { "\x1B\x2D\x47", "iso-ir-127" },
+    { "\x1B\x2D\x46", "iso-ir-126" },
+    { "\x1B\x2D\x48", "iso-ir-138" },
+    { "\x1B\x2D\x4D", "iso-ir-148" },
+    { "\x1B\x29\x49", "ISO-2022-JP" },
+    { "\x1B\x28\x4A", "ISO-2022-JP" },
+    { "\x1B\x2D\x54", "TIS-620" },
+
+    // Multi-Byte Character Sets with Code Extensions
+    { "ISO 2022 IR 87", "ISO-2022-JP" },
+    { "ISO 2022 IR 149", "EUC-KR" },
+    { "ISO 2022 IR 58", "ISO-2022-CN" },
+
+    { "\x1B\x24\x42", "ISO-2022-JP" },
+    { "\x1B\x24\x28\x44", "ISO-2022-JP" },
+    { "\x1B\x24\x29\x43", "iso-ir-149" },
+    { "\x1B\x24\x29\x41", "iso-ir-58" },
+
+    // Multi-Byte Character Sets Without Code Extensions
+    { "ISO_IR 192", "UTF-8" },
+    { "GB18030", "GB18030" },
+    // GBK
+};
+
+std::map<std::string, std::vector<std::string>> const escape_sequences = {
+    { "",   { "\x1B\x28\x42" } },
+    { "ISO 2022 IR 6",   { "\x1B\x28\x42" } },
+    { "ISO 2022 IR 100", { "\x1B\x2D\x41", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 101", { "\x1B\x2D\x42", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 109", { "\x1B\x2D\x43", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 110", { "\x1B\x2D\x44", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 144", { "\x1B\x2D\x4C", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 127", { "\x1B\x2D\x47", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 126", { "\x1B\x2D\x46", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 138", { "\x1B\x2D\x48", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 148", { "\x1B\x2D\x4D", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 13",  { "\x1B\x29\x49", "\x1B\x28\x4A" } },
+    { "ISO 2022 IR 166", { "\x1B\x2D\x54", "\x1B\x28\x42" } },
+
+    { "ISO 2022 IR 87",  { "\x1B\x24\x42", "\x1B\x28\x42" } },
+    { "ISO 2022 IR 159", { "\x1B\x24\x28\x44" } },
+    { "ISO 2022 IR 149", { "\x1B\x24\x29\x43" } },
+    { "ISO 2022 IR 58",  { "\x1B\x24\x29\x41" } },
+};
+
+std::string find_encoder(std::string const & specific_character_set)
+{
+    std::string encoder;
+    auto const it = icu_encodings.find(specific_character_set);
+    if(it == icu_encodings.end())
+    {
+        throw Exception("Unknown encoding: "+specific_character_set);
+    }
+    encoder = it->second;
+
+    return encoder;
+}
+
+std::string as_utf8(
+    std::string::const_iterator const begin, std::string::const_iterator const end,
+    std::string const & encoding)
+{
+    icu::UnicodeString unicode(&(*begin), end-begin, encoding.c_str());
+    std::string result;
+    unicode.toUTF8String(result);
+
+    return result;
+}
+
+std::string as_utf8(
+    std::string::const_iterator const begin, std::string::const_iterator const end,
+    std::string const & initial_encoder,
+    std::vector<std::string> const & active_escape_sequences)
+{
+    std::string encoded;
+
+    auto encoder = initial_encoder;
+
+    auto item_begin = begin;
+    while(item_begin != end)
+    {
+        auto const item_end = std::find(item_begin+1, end, '\x1B');
+
+        if(*item_begin == '\x1B')
+        {
+            std::string escape_sequence(item_begin, item_begin+3);
+
+            // Find an encoder for the escape sequence
+            if(
+                std::find(
+                active_escape_sequences.begin(), active_escape_sequences.end(),
+                escape_sequence) != active_escape_sequences.end())
+            {
+                encoder = find_encoder(escape_sequence);
+            }
+            else
+            {
+                escape_sequence += *(item_begin+3);
+                if(
+                    std::find(
+                    active_escape_sequences.begin(), active_escape_sequences.end(),
+                    escape_sequence) != active_escape_sequences.end())
+                {
+                    encoder = find_encoder(escape_sequence);
+                }
+                else
+                {
+                    // Unknown escape sequence: do not update the encoder,
+                    // hope for the best.
+                }
+            }
+
+            if(encoder != "ISO-2022-JP")
+            {
+                // The ISO-2022-JP encoder of ICU must have the escape
+                // sequences
+                item_begin += escape_sequence.size();
+            }
+        }
+
+        encoded += as_utf8(item_begin, item_end, encoder);
+
+        item_begin = item_end;
+    }
+
+    return encoded;
+}
+
+enum class Group
+{
+    Alphabetic,
+    Ideographic,
+    Phonetic
+};
+
+std::vector<std::string> get_active_escape_sequences(
+    Value::Strings const & specific_character_set, Group const & group)
+{
+    std::vector<std::string> active_escape_sequences;
+
+    if(specific_character_set.size() >= 2)
+    {
+        auto const escape_sequences_it = escape_sequences.find(specific_character_set[1]);
+        if(escape_sequences_it == escape_sequences.end())
+        {
+            throw Exception("Unknown specific character set: "+specific_character_set[1]);
+        }
+        std::copy(
+            escape_sequences_it->second.begin(),
+            escape_sequences_it->second.end(),
+            std::back_inserter(active_escape_sequences));
+    }
+    if(group == Group::Phonetic)
+    {
+        auto const escape_sequences_it = escape_sequences.find(specific_character_set[0]);
+        if(escape_sequences_it != escape_sequences.end())
+        {
+            std::copy(
+                escape_sequences_it->second.begin(),
+                escape_sequences_it->second.end(),
+                std::back_inserter(active_escape_sequences));
+        }
+        if(specific_character_set.size() >= 3)
+        {
+            auto const escape_sequences_it = escape_sequences.find(specific_character_set[2]);
+            if(escape_sequences_it == escape_sequences.end())
+            {
+                throw Exception("Unknown specific character set: "+specific_character_set[2]);
+            }
+            std::copy(
+                escape_sequences_it->second.begin(),
+                escape_sequences_it->second.end(),
+                std::back_inserter(active_escape_sequences));
+        }
+    }
+
+    return active_escape_sequences;
+}
+
+std::string as_utf8(
+    std::string const & input, Value::Strings const & specific_character_set,
+    bool is_pn)
+{
+    // Control characters: line feed, carriage return, form feed and tabulation
+    // For Person Name, add the group splitters
+    std::string splitters = "\n\r\f\t";
+    if(is_pn)
+    {
+        if(
+            std::find(
+                specific_character_set.begin(), specific_character_set.end(),
+                "ISO 2022 IR 13") != specific_character_set.end() ||
+            std::find(
+                specific_character_set.begin(), specific_character_set.end(),
+                "ISO 2022 IR 87") != specific_character_set.end())
+        {
+            // The ISO-2022-JP encoder of ICU is stateful, whereas all other
+            // ISO 2022 encoders seem to be only codepages (i.e. stateless)
+            splitters.append("=");
+        }
+        else
+        {
+            splitters.append("^=");
+        }
+    }
+
+    std::string result;
+
+    Group group=Group::Alphabetic;
+
+    auto begin = input.begin();
+    while(begin != input.end())
+    {
+        // Active character set resets to default before any of the splitters
+        // cf. PS 3.5, 6.1.2.5.3
+        auto const end = std::find_first_of(
+            begin, input.end(), splitters.begin(), splitters.end());
+
+        std::string encoded;
+
+        if(specific_character_set.empty())
+        {
+            encoded = std::string(begin, end);
+        }
+        else if(is_pn && group != Group::Alphabetic)
+        {
+            auto const initial_encoder = find_encoder(specific_character_set[0]);
+            auto const active_escape_sequences =
+                get_active_escape_sequences(specific_character_set, group);
+            encoded = as_utf8(begin, end, initial_encoder, active_escape_sequences);
+        }
+        else
+        {
+            auto const encoder = find_encoder(specific_character_set[0]);
+            encoded = as_utf8(begin, end, encoder);
+        }
+
+        result.append(encoded);
+
+        // If present, add the splitter to the UTF-8 string.
+        if(end != input.end())
+        {
+            result.push_back(*end);
+            begin = end+1;
+            if(is_pn && *end == '=')
+            {
+                if(group == Group::Alphabetic)
+                {
+                    group = Group::Ideographic;
+                }
+                else if(group == Group::Ideographic)
+                {
+                    group = Group::Phonetic;
+                }
+                else
+                {
+                    throw Exception("Too many groups");
+                }
+            }
+        }
+        else
+        {
+            begin = end;
+        }
+    }
+
+    return result;
+}
+
+}
---
+++ tests/code/unicode.cpp
@@ -0,0 +1,258 @@
+#define BOOST_TEST_MODULE unicode
+#include <boost/test/unit_test.hpp>
+
+#include <dcmtk/config/osconfig.h>
+#include <dcmtk/dcmdata/dctk.h>
+
+#include "dcmtkpp/conversion.h"
+#include "dcmtkpp/DataSet.h"
+#include "dcmtkpp/unicode.h"
+
+BOOST_AUTO_TEST_CASE(SCSARAB)
+{
+    dcmtkpp::Value::Strings const specific_character_set = { "ISO_IR 127" };
+    std::string const source =
+        "\xe2\xc8\xc7\xe6\xea" "^" "\xe4\xe6\xd2\xc7\xd1";
+    std::string const expected =
+        "\xd9\x82\xd8\xa8\xd8\xa7\xd9\x86\xd9\x8a"
+        "^"
+        "\xd9\x84\xd9\x86\xd8\xb2\xd8\xa7\xd8\xb1";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSFREN)
+{
+    dcmtkpp::Value::Strings const specific_character_set = { "ISO_IR 100" };
+    std::string const source = "Buc" "^" "J\xe9r\xf4me";
+    std::string const expected =
+        "Buc" "^" "J\xc3\xa9r\xc3\xb4me";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSGERM)
+{
+    dcmtkpp::Value::Strings const specific_character_set = { "ISO_IR 100" };
+    std::string const source =
+        "\xc4neas" "^" "R\xfc" "diger";
+    std::string const expected =
+        "\xc3\x84neas" "^" "R\xc3\xbc" "diger";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSGREEK)
+{
+    dcmtkpp::Value::Strings const specific_character_set = { "ISO_IR 126" };
+    std::string const source = "\xc4\xe9\xef\xed\xf5\xf3\xe9\xef\xf2";
+    std::string const expected =
+        "\xce\x94\xce\xb9\xce\xbf\xce\xbd\xcf\x85\xcf\x83\xce\xb9\xce\xbf\xcf\x82";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSH31)
+{
+    dcmtkpp::Value::Strings const specific_character_set =
+        { "", "ISO 2022 IR 87" };
+    std::string const source =
+        "Yamada" "^" "Tarou"
+        "="
+        "\x1b\x24\x42\x3b\x33\x45\x44\x1b\x28\x42"
+            "^" "\x1b\x24\x42\x42\x40\x4f\x3a\x1b\x28\x42"
+        "="
+        "\x1b\x24\x42\x24\x64\x24\x5e\x24\x40\x1b\x28\x42"
+            "^" "\x1b\x24\x42\x24\x3f\x24\x6d\x24\x26\x1b\x28\x42";
+    std::string const expected =
+        "Yamada" "^" "Tarou"
+        "="
+        "\xe5\xb1\xb1\xe7\x94\xb0" "^" "\xe5\xa4\xaa\xe9\x83\x8e"
+        "="
+        "\xe3\x82\x84\xe3\x81\xbe\xe3\x81\xa0"
+            "^" "\xe3\x81\x9f\xe3\x82\x8d\xe3\x81\x86";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSH32)
+{
+    dcmtkpp::Value::Strings const specific_character_set =
+        { "ISO 2022 IR 13", "ISO 2022 IR 87" };
+    std::string const source =
+        "\xd4\xcf\xc0\xde" "^" "\xc0\xdb\xb3"
+        "="
+        "\x1b\x24\x42\x3b\x33\x45\x44\x1b\x28\x4a"
+            "^" "\x1b\x24\x42\x42\x40\x4f\x3a\x1b\x28\x4a"
+        "="
+        "\x1b\x24\x42\x24\x64\x24\x5e\x24\x40\x1b\x28\x4a"
+            "\x5e\x1b\x24\x42\x24\x3f\x24\x6d\x24\x26\x1b\x28\x4a";
+    std::string const expected =
+        "\xef\xbe\x94\xef\xbe\x8f\xef\xbe\x80\xef\xbe\x9e"
+            "^" "\xef\xbe\x80\xef\xbe\x9b\xef\xbd\xb3"
+        "="
+        "\xe5\xb1\xb1\xe7\x94\xb0"
+            "^" "\xe5\xa4\xaa\xe9\x83\x8e"
+        "="
+        "\xe3\x82\x84\xe3\x81\xbe\xe3\x81\xa0"
+            "^" "\xe3\x81\x9f\xe3\x82\x8d\xe3\x81\x86";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSHBRW)
+{
+    dcmtkpp::Value::Strings const specific_character_set = { "ISO_IR 138" };
+    std::string const source = "\xf9\xf8\xe5\xef" "^" "\xe3\xe1\xe5\xf8\xe4";
+    std::string const expected =
+        "\xd7\xa9\xd7\xa8\xd7\x95\xd7\x9f"
+            "^" "\xd7\x93\xd7\x91\xd7\x95\xd7\xa8\xd7\x94";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSI2)
+{
+    dcmtkpp::Value::Strings const specific_character_set =
+        { "", "ISO 2022 IR 149" };
+
+    std::string const source =
+        "\x48\x6f\x6e\x67" "^" "\x47\x69\x6c\x64\x6f\x6e\x67"
+        "="
+        "\x1b\x24\x29\x43\xfb\xf3" "^" "\x1b\x24\x29\x43\xd1\xce\xd4\xd7"
+        "="
+        "\x1b\x24\x29\x43\xc8\xab" "^" "\x1b\x24\x29\x43\xb1\xe6\xb5\xbf";
+    std::string const expected =
+        "\x48\x6f\x6e\x67" "^" "\x47\x69\x6c\x64\x6f\x6e\x67"
+        "="
+        "\xe6\xb4\xaa" "^" "\xe5\x90\x89\xe6\xb4\x9e"
+        "="
+        "\xed\x99\x8d" "^" "\xea\xb8\xb8\xeb\x8f\x99";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSRUSS)
+{
+    dcmtkpp::Value::Strings const specific_character_set = { "ISO_IR 144" };
+    std::string const source = "\xbb\xee\xda\x63\x65\xdc\xd1\x79\x70\xd3";
+    std::string const expected =
+        "\xd0\x9b\xd1\x8e\xd0\xba\x63\x65\xd0\xbc\xd0\xb1\x79\x70\xd0\xb3";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSX1)
+{
+    dcmtkpp::Value::Strings const specific_character_set = { "ISO_IR 192" };
+    std::string const source =
+        "Wang" "^" "XiaoDong"
+        "="
+        "\xe7\x8e\x8b" "^" "\xe5\xb0\x8f\xe6\x9d\xb1"
+        "=";
+    std::string const expected =
+        "Wang" "^" "XiaoDong"
+        "\x3d\xe7\x8e\x8b" "^" "\xe5\xb0\x8f\xe6\x9d\xb1"
+        "=";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+BOOST_AUTO_TEST_CASE(SCSX2)
+{
+    dcmtkpp::Value::Strings const specific_character_set = { "GB18030" };
+    std::string const source =
+        "Wang" "^" "XiaoDong"
+        "=" ""
+        "\xcd\xf5" "^" "\xd0\xa1\xb6\xab"
+        "=";
+    std::string const expected =
+        "Wang" "^" "XiaoDong"
+        "="
+        "\xe7\x8e\x8b" "^" "\xe5\xb0\x8f\xe4\xb8\x9c"
+        "=";
+
+    std::string const utf8 = dcmtkpp::as_utf8(
+        source, specific_character_set, true);
+    BOOST_REQUIRE_EQUAL(utf8, expected);
+}
+
+/*
+dcmtkpp::DataSet read(std::string const & filename)
+{
+    DcmFileFormat format;
+
+    OFCondition const condition = format.loadFile(filename.c_str());
+    if(condition.bad())
+    {
+        throw dcmtkpp::Exception(condition);
+    }
+
+    dcmtkpp::DataSet const result = dcmtkpp::convert(format.getDataset());
+
+    return result;
+}
+
+
+int main(int argc, char ** argv)
+{
+    for(char** argument=argv+1; argument != argv+argc; ++argument)
+    {
+        std::cout << "Reading " << *argument << std::endl;
+
+        dcmtkpp::DataSet data_set;
+        try
+        {
+            data_set = read(*argument);
+        }
+        catch(dcmtkpp::Exception const & e)
+        {
+            std::cerr << "Could not read " << *argument << ": " << e.what() << "\n";
+            continue;
+        }
+
+        std::string patient_name;
+        try
+        {
+            patient_name = dcmtkpp::as_utf8(
+                data_set.as_string("PatientName")[0],
+                data_set.as_string("SpecificCharacterSet"), true);
+        }
+        catch(dcmtkpp::Exception const & e)
+        {
+            std::cerr << "Could not convert PatientName to UTF-8: " << e.what() << "\n";
+            continue;
+        }
+
+        std::cout << "    dcmtkpp::Value::Strings const specific_character_set = {\n";
+        for(auto const & c: data_set.as_string("SpecificCharacterSet"))
+        {
+            std::cout << "        " << "\"" << c << "\",\n";
+        }
+        std::cout << "    };\n";
+        std::cout << "Patient Name: \"" << patient_name << "\"\n";
+    }
+
+    return 0;
+}
+*/
