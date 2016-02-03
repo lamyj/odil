667a2d28902af2df358680196594e1781893a9ae
Julien Lamy <lamy@unistra.fr>
2016-02-03 19:03:42 +0100

    Add "add" wrappers to DataSet

--- tests/wrappers/test_data_set.py
+++ tests/wrappers/test_data_set.py
@@ -6,6 +6,92 @@ class TestDataSet(unittest.TestCase):
     def test_empty_constructor(self):
         data_set = _odil.DataSet()
         self.assertTrue(data_set.empty())
+        self.assertEqual(data_set.size(), 0)
+
+    def test_empty_element(self):
+        tag = _odil.Tag("PatientName")
+        data_set = _odil.DataSet()
+        data_set.add(tag, _odil.Element())
+
+        self.assertFalse(data_set.empty())
+        self.assertEqual(data_set.size(), 1)
+
+        self.assertEqual(data_set.get_vr(tag), _odil.VR.INVALID)
+        self.assertTrue(data_set.empty(tag))
+        self.assertEqual(data_set.size(tag), 0)
+
+    def test_empty_element_tag(self):
+        tag = _odil.Tag("PatientName")
+        data_set = _odil.DataSet()
+        data_set.add(tag)
+
+        self.assertFalse(data_set.empty())
+        self.assertEqual(data_set.size(), 1)
+
+        self.assertEqual(data_set.get_vr(tag), _odil.VR.PN)
+        self.assertTrue(data_set.empty(tag))
+        self.assertEqual(data_set.size(tag), 0)
+
+    def test_int_element(self):
+        tag = _odil.Tag("SelectorUSValue")
+        data_set = _odil.DataSet()
+        data_set.add(tag, _odil.Value.Integers([1,2,3]))
+
+        self.assertFalse(data_set.empty())
+        self.assertEqual(data_set.size(), 1)
+
+        self.assertEqual(data_set.get_vr(tag), _odil.VR.US)
+        self.assertFalse(data_set.empty(tag))
+        self.assertEqual(data_set.size(tag), 3)
+
+    def test_real_element(self):
+        tag = _odil.Tag("SelectorFLValue")
+        data_set = _odil.DataSet()
+        data_set.add(tag, _odil.Value.Reals([1.1, 2, 3.3]))
+
+        self.assertFalse(data_set.empty())
+        self.assertEqual(data_set.size(), 1)
+
+        self.assertEqual(data_set.get_vr(tag), _odil.VR.FL)
+        self.assertFalse(data_set.empty(tag))
+        self.assertEqual(data_set.size(tag), 3)
+
+    def test_string_element(self):
+        tag = _odil.Tag("SelectorCSValue")
+        data_set = _odil.DataSet()
+        data_set.add(tag, _odil.Value.Strings(["foo", "bar"]))
+
+        self.assertFalse(data_set.empty())
+        self.assertEqual(data_set.size(), 1)
+
+        self.assertEqual(data_set.get_vr(tag), _odil.VR.CS)
+        self.assertFalse(data_set.empty(tag))
+        self.assertEqual(data_set.size(tag), 2)
+
+    def test_data_set_element(self):
+        tag = _odil.Tag("SelectorCodeSequenceValue")
+        data_set = _odil.DataSet()
+        data_set.add(
+            tag, _odil.Value.DataSets([_odil.DataSet(), _odil.DataSet()]))
+
+        self.assertFalse(data_set.empty())
+        self.assertEqual(data_set.size(), 1)
+
+        self.assertEqual(data_set.get_vr(tag), _odil.VR.SQ)
+        self.assertFalse(data_set.empty(tag))
+        self.assertEqual(data_set.size(tag), 2)
+
+    def test_string_binary(self):
+        tag = _odil.Tag("RedPaletteColorLookupTableData")
+        data_set = _odil.DataSet()
+        data_set.add(tag, _odil.Value.Binary("\x01\x02"))
+
+        self.assertFalse(data_set.empty())
+        self.assertEqual(data_set.size(), 1)
+
+        self.assertEqual(data_set.get_vr(tag), _odil.VR.OW)
+        self.assertFalse(data_set.empty(tag))
+        self.assertEqual(data_set.size(tag), 2)
 
 if __name__ == "__main__":
     unittest.main()
--- wrappers/DataSet.cpp
+++ wrappers/DataSet.cpp
@@ -11,21 +11,64 @@
 
 #include "odil/DataSet.h"
 
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(add_overloads_2, DataSet::add, 1, 2);
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(add_overloads_3, DataSet::add, 2, 3);
+
 void wrap_DataSet()
 {
     using namespace boost::python;
     using namespace odil;
 
-    //BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(DataSet_overloads, add, 2, 3)
+
     class_<DataSet>("DataSet", init<>())
-        //.def("add", &DataSet::add, DataSet_overloads())
+        .def(
+            "add", 
+            static_cast<void (DataSet::*)(Tag const &, Element const &)>(
+                &DataSet::add))
+        .def(
+            "add", 
+            static_cast<void (DataSet::*)(Tag const &, VR)>(&DataSet::add),
+            add_overloads_2())
+        .def(
+            "add", 
+            static_cast<void (DataSet::*)(Tag const &, Value::Integers const &, VR)>(
+                &DataSet::add),
+            add_overloads_3())
+        .def(
+            "add", 
+            static_cast<void (DataSet::*)(Tag const &, Value::Reals const &, VR)>(
+                &DataSet::add),
+            add_overloads_3())
+        .def(
+            "add", 
+            static_cast<void (DataSet::*)(Tag const &, Value::Strings const &, VR)>(
+                &DataSet::add),
+            add_overloads_3())
+        .def(
+            "add", 
+            static_cast<void (DataSet::*)(Tag const &, Value::DataSets const &, VR)>(
+                &DataSet::add),
+            add_overloads_3())
+        .def(
+            "add", 
+            static_cast<void (DataSet::*)(Tag const &, Value::Binary const &, VR)>(
+                &DataSet::add),
+            add_overloads_3())
         .def("remove", &DataSet::remove)
         .def("has", &DataSet::has)
-        .def("get_vr", &DataSet::get_vr)
         .def("empty", static_cast<bool (DataSet::*)() const>(&DataSet::empty))
         .def(
             "size", 
             static_cast<std::size_t (DataSet::*)() const>(&DataSet::size))
+        .def("get_vr", &DataSet::get_vr)
+        .def(
+            "empty", 
+            static_cast<bool (DataSet::*)(Tag const &) const>(&DataSet::empty))
+        .def(
+            "size", 
+            static_cast<std::size_t (DataSet::*)(Tag const &) const>(
+                &DataSet::size))
         // operator[], cf. map
         // is_XXX, as_XXX
         // iteration
