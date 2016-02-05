477405b4e22051f56549311b7d19254f461d29cb
Julien Lamy <lamy@unistra.fr>
2016-02-05 21:14:42 +0100

    Add registry wrapper

--- wrappers/registry.cpp
+++ wrappers/registry.cpp
@@ -8,27 +8,26 @@
 
 #include <boost/python.hpp>
 
-void wrap_DataSet();
-void wrap_Element();
-void wrap_Exception();
-void wrap_read();
-void wrap_Tag();
-void wrap_uid();
-void wrap_Value();
-void wrap_VR();
+#include <odil/registry.h>
 
-void wrap_registry();
-
-BOOST_PYTHON_MODULE(_odil)
+namespace
 {
-    wrap_DataSet();
-    wrap_Element();
-    wrap_Exception();
-    wrap_read();
-    wrap_Tag();
-    wrap_uid();
-    wrap_Value();
-    wrap_VR();
 
-    wrap_registry();
+class Dummy 
+{
+};
+
+}
+
+void wrap_registry()
+{
+    using namespace boost::python;
+    using namespace odil;
+
+    scope registry_scope = class_<Dummy>("registry");
+
+    for(auto const & entry: registry::public_dictionary)
+    {
+        registry_scope.attr(entry.second.keyword.c_str()) = entry.first;
+    }
 }
