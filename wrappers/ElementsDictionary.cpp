f32c092745d61d82c4058111bc1954abc45d871b
Julien Lamy <lamy@unistra.fr>
2016-03-22 08:37:41 +0100

    Add polymorphy to ElementsDictionary

--- wrappers/ElementsDictionary.cpp
+++ wrappers/ElementsDictionary.cpp
@@ -11,10 +11,55 @@
 
 #include <odil/ElementsDictionary.h>
 
+namespace
+{
+
+bool contains(odil::ElementsDictionary const & container, odil::Tag const & key)
+{
+    return container.find(key) != container.end();
+}
+
+bool contains(odil::ElementsDictionary const & container, std::string const & key)
+{
+    return container.find(key) != container.end();
+}
+
+odil::ElementsDictionaryEntry &
+get_item(odil::ElementsDictionary & container, odil::Tag const & key)
+{
+    odil::ElementsDictionary::iterator iterator = container.find(key);
+    if(iterator == container.end())
+    {
+        PyErr_SetString(PyExc_KeyError, "Invalid key");
+        boost::python::throw_error_already_set();
+    }
+    return iterator->second;
+}
+
+odil::ElementsDictionaryEntry &
+get_item(odil::ElementsDictionary & container, std::string const & key)
+{
+    odil::ElementsDictionary::iterator iterator = container.find(key);
+    if(iterator == container.end())
+    {
+        PyErr_SetString(PyExc_KeyError, "Invalid key");
+        boost::python::throw_error_already_set();
+    }
+    return iterator->second;
+}
+
+
+}
+
 void wrap_ElementsDictionary()
 {
     using namespace boost::python;
     using namespace odil;
+
+    class_<ElementsDictionaryKey>("ElementsDictionaryKey", init<>())
+        .def(init<Tag>())
+        .def(init<std::string>())
+    ;
 
     class_<ElementsDictionaryEntry>(
         "ElementsDictionaryEntry",
@@ -28,6 +73,24 @@ void wrap_ElementsDictionary()
 
     class_<ElementsDictionary>("ElementsDictionary")
         .def(map_indexing_suite<ElementsDictionary>())
+        .def(
+            "__contains__",
+            static_cast<bool(*)(ElementsDictionary const &, Tag const &)>(contains)
+        )
+        .def(
+            "__contains__",
+            static_cast<bool(*)(ElementsDictionary const &, std::string const &)>(contains)
+        )
+        .def(
+            "__getitem__",
+            static_cast<ElementsDictionaryEntry & (*)(ElementsDictionary &, Tag const &)>(get_item),
+            return_value_policy<reference_existing_object>()
+        )
+        .def(
+            "__getitem__",
+            static_cast<ElementsDictionaryEntry & (*)(ElementsDictionary &, std::string const &)>(get_item),
+            return_value_policy<reference_existing_object>()
+        )
+        ;
     ;
 }
-
