cb8d0439e7f1d469e1ce36879ed61ec0bb383c5d
Julien Lamy <lamy@unistra.fr>
2016-03-31 08:34:04 +0200

    Add find in application

---
+++ applications/find.py
@@ -0,0 +1,70 @@
+import logging
+
+import _odil
+
+from print_ import print_data_set
+
+def add_subparser(subparsers):
+    parser = subparsers.add_parser(
+        "find", help="DICOM query (C-FIND)")
+    parser.add_argument("host")
+    parser.add_argument("port", type=int)
+    parser.add_argument("calling_ae_title")
+    parser.add_argument("called_ae_title")
+    parser.add_argument("level", choices=["patient", "study"])
+    parser.add_argument("keys", nargs="+")
+    parser.set_defaults(function=find)
+
+def find(host, port, calling_ae_title, called_ae_title, level, keys):
+    query = _odil.DataSet()
+    for key in keys:
+        if "=" in key:
+            key, value = key.split("=", 1)
+            value = value.split("\\")
+        else:
+            value = None
+        
+        tag = getattr(_odil.registry, key)
+        
+        if value is not None:
+            vr = _odil.registry.public_dictionary[tag].vr
+            if vr in ["DS", "FL", "FD"]:
+                value = _odil.Value.Reals([float(x) for x in value])
+            elif vr in ["IS", "SL", "SS", "UL", "US"]:
+                value = _odil.Value.Integers([int(x) for x in value])
+            else:
+                value = _odil.Value.Strings(value)
+                
+            query.add(tag, value)
+        else:
+            query.add(tag)
+    
+    sop_class = getattr(
+        _odil.registry, 
+        "{}RootQueryRetrieveInformationModelFIND".format(level.capitalize()))
+    
+    find_pc = _odil.AssociationParameters.PresentationContext(
+        1, sop_class,
+        [ _odil.registry.ExplicitVRLittleEndian ], True, False
+    )
+    
+    association = _odil.Association()
+    association.set_peer_host(host)
+    association.set_peer_port(port)
+    association.update_parameters()\
+        .set_calling_ae_title(calling_ae_title)\
+        .set_called_ae_title(called_ae_title) \
+        .set_presentation_contexts([find_pc])
+    association.associate()
+    logging.info("Association established")
+    
+    find = _odil.FindSCU(association)
+    find.set_affected_sop_class(sop_class)
+    data_sets = find.find(query)
+    print "{} answer{}".format(len(data_sets), "s" if len(data_sets)>1 else "")
+    for data_set in data_sets:
+        print_data_set(data_set)
+        print
+
+    association.release()
+    logging.info("Association released")
