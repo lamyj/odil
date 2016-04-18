304d20775a6d0011c2a927b34ea022b2a5a61ee2
Julien Lamy <lamy@unistra.fr>
2016-04-18 19:11:55 +0200

    Improve exception wrappers

--- wrappers/exception_factory.h
+++ wrappers/exception_factory.h
@@ -6,26 +6,9 @@
  * for details.
  ************************************************************************/
 
+#include <string>
 #include <boost/python.hpp>
 
-#include "odil/Exception.h"
-
-#include "exception_factory.h"
-
-namespace
-{
-
-PyObject * wrapped_Exception;
-
-void translator(odil::Exception const & e)
-{
-    PyErr_SetString(wrapped_Exception, e.what());
-}
-
-}
-
-void wrap_Exception()
-{
-    wrapped_Exception = exception_factory("Exception");
-    boost::python::register_exception_translator<odil::Exception>(translator);
-}
+/// @brief Create a new Python exception type, in the current scope.
+PyObject * exception_factory(
+    std::string const & name, PyObject * parent=PyExc_Exception);
