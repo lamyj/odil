e01c2b80735038ffbf5da8dc560fab678af351aa
Christophe Guébert <christophe.guebert@gmail.com>
2016-04-27 10:52:02 +0200

    Fix finding all DCMTK libraries.

--- FindDCMTK.cmake
+++ FindDCMTK.cmake
@@ -9,11 +9,14 @@
 set(DCMTK_DEFINITIONS "-D HAVE_CONFIG_H")
 
 find_path(DCMTK_INCLUDE_DIR "dcmtk/dcmdata/dctk.h")
-find_library(DCMTK_LIBRARY dcmdata)
+find_library(DCMNET_LIBRARY dcmnet)
+find_library(DCMDATA_LIBRARY dcmdata)
+find_library(OFLOG_LIBRARY oflog)
+find_library(OFSTD_LIBRARY ofstd)
 
 set(DCMTK_INCLUDE_DIRS ${DCMTK_INCLUDE_DIR})
+set(DCMTK_LIBRARIES ${DCMNET_LIBRARY} ${DCMDATA_LIBRARY} ${OFLOG_LIBRARY} ${OFSTD_LIBRARY})
 
-set(DCMTK_LIBRARIES ${DCMTK_LIBRARY} dcmnet dcmdata oflog ofstd)
 foreach(library iconv pthread wrap z)
     find_library(${library}_LIBRARY ${library})
     if(${library}_LIBRARY)
@@ -26,7 +29,7 @@ get_filename_component(DCMTK_LIBRARY_DIRS "${DCMTK_LIBRARY}" PATH)
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set DCMTK_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(
-    DCMTK DEFAULT_MSG DCMTK_INCLUDE_DIR DCMTK_LIBRARY)
+find_package_handle_standard_args(DCMTK DEFAULT_MSG 
+	DCMTK_INCLUDE_DIR DCMNET_LIBRARY DCMDATA_LIBRARY OFLOG_LIBRARY OFSTD_LIBRARY)
 
-mark_as_advanced(DCMTK_INCLUDE_DIR DCMTK_LIBRARY)
+mark_as_advanced(DCMTK_INCLUDE_DIR DCMNET_LIBRARY DCMDATA_LIBRARY OFLOG_LIBRARY OFSTD_LIBRARY)
