48000f30c0aba22acf664fa90afc65f125ac9023
Thibaut Schmitt <thibaut.schmitt@ihu-strasbourg.eu>
2017-02-10 10:54:44 +0100

    style(wrapper): SCPDispatcher clean dead code.

--- wrappers/SCPDispatcher.cpp
+++ wrappers/SCPDispatcher.cpp
@@ -17,54 +17,9 @@
 namespace
 {
 
-void set_scp_in_python( odil::SCPDispatcher& scpDispatcher,  odil::Value::Integer command
-                        , ::boost::python::object& scp_to_add )
-{
-    try
-    {
-        /*
-           auto scp = dynamic_cast< odil::SCP& >(scp_to_add);
-           auto shared_scp = std::make_shared<odil::SCP>(scp);
-           scpDispatcher.set_scp(command, shared_scp);
-         */
-    }
-    catch(...)
-    {
-        //Fuck it
-    }
-}
-
 void dispatch_in_python( odil::SCPDispatcher& dispatcher)
 {
     dispatcher.dispatch();
-}
-
-void functionTest(odil::SCPDispatcher& scpd)
-{
-    std::cout << "Hello" << std::endl << std::endl << std::endl << std::endl << std::endl << std::endl;
-}
-
-void set_storeSCP(odil::SCPDispatcher& scpd, odil::StoreSCP scp)
-{
-    auto shared_scp              = std::make_shared< odil::StoreSCP>( scp );
-    odil::Value::Integer command = ::odil::message::Message::Command::Type::C_STORE_RQ;
-
-    scpd.set_scp(command, shared_scp );
-}
-
-void set_nsetSCP(odil::SCPDispatcher& scpd, odil::NSetSCP scp)
-{
-    auto shared_scp              = std::make_shared< odil::NSetSCP>( scp );
-    odil::Value::Integer command = ::odil::message::Message::Command::Type::N_SET_RQ;
-    scpd.set_scp( command, shared_scp );
-}
-
-void set_echoSCP(odil::SCPDispatcher& scpd, odil::EchoSCP scp)
-{
-    auto shared_scp              = std::make_shared< odil::EchoSCP>( scp );
-    odil::Value::Integer command = ::odil::message::Message::Command::Type::C_ECHO_RQ;
-
-    scpd.set_scp(command, shared_scp );
 }
 
 template<typename TSCP, odil::message::Message::Command::Type Command>
@@ -82,15 +37,11 @@ void wrap_SCPDispatcher()
 
     class_<SCPDispatcher >("SCPDispatcher", init<Association&>())
     .def("set_scp", &SCPDispatcher::set_scp )
-//            .def("add_scp", &set_scp_in_python )
     .def("dispatch", &dispatch_in_python )
-    //.def("set_echoSCP", &set_echoSCP)
     .def("set_echo_scp", &set_scp<EchoSCP, message::Message::Command::Type::C_ECHO_RQ>)
-    .def("set_storeSCP", &set_storeSCP)
-    .def("set_nsetSCP", &set_nsetSCP)
+    .def("set_store_scp", &set_scp<StoreSCP, message::Message::Command::Type::C_STORE_RQ>)
+    .def("set_nset_scp", &set_scp<NSetSCP, message::Message::Command::Type::N_SET_RQ>)
     ;
-
-
 }
 
 
