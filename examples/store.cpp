eb58411124c910588a6d55c71a42f629e5cbde1d
Julien Lamy <lamy@unistra.fr>
2017-02-20 16:13:55 +0100

    Revert store example

--- examples/store.cpp
+++ examples/store.cpp
@@ -7,42 +7,37 @@
 int main(int argc, char** argv)
 {
     odil::Association association;
-//    association.set_peer_host("184.73.255.26");
-    association.set_peer_host("127.0.0.1");
+    association.set_peer_host("184.73.255.26");
     association.set_peer_port(11112);
     association.update_parameters()
-    .set_calling_ae_title("myself")
-    .set_called_ae_title("AWSPIXELMEDPUB")
-    .set_presentation_contexts({
-        {
-            1, odil::registry::MRImageStorage,
-            { odil::registry::ImplicitVRLittleEndian }, false, true
-        },
-        {
-            3, odil::registry::VerificationSOPClass,
-            { odil::registry::ImplicitVRLittleEndian }, true, false
-        },
-        {
-            5, odil::registry::XRayAngiographicImageStorage,
-            { odil::registry::ImplicitVRLittleEndian }, true, false
-        }
-    });
-
+        .set_calling_ae_title("myself")
+        .set_called_ae_title("AWSPIXELMEDPUB")
+        .set_presentation_contexts({
+            {
+                1, odil::registry::MRImageStorage,
+                { odil::registry::ImplicitVRLittleEndian }, false, true
+            },
+            {
+                3, odil::registry::VerificationSOPClass,
+                { odil::registry::ImplicitVRLittleEndian }, true, false
+            }
+        });
+    
     association.associate();
 
     odil::EchoSCU echo_scu(association);
     echo_scu.echo();
-
+    
     odil::StoreSCU scu(association);
-
-    for(int i = 1; i<argc; ++i)
+    
+    for(int i=1; i<argc; ++i)
     {
         std::ifstream stream(argv[i], std::ios::binary);
         auto const header_and_data_set = odil::Reader::read_file(stream);
-
+        
         scu.set_affected_sop_class(header_and_data_set.first);
         scu.store(header_and_data_set.first);
     }
-
+    
     association.release();
 }
