65b691cf4443f129bc87b4ecab1a630ee67eb176
Julien Lamy <lamy@unistra.fr>
2017-03-04 18:55:36 +0100

    Separate headers wrapping

--- wrappers/webservices/Message.cpp
+++ wrappers/webservices/Message.cpp
@@ -7,51 +7,13 @@
  ************************************************************************/
 
 #include <boost/python.hpp>
-#include <boost/python/suite/indexing/map_indexing_suite.hpp>
 
 #include "odil/webservices/Message.h"
 
+#include "headers.h"
+
 namespace
 {
-
-boost::python::dict
-convert_headers(odil::webservices::Message::Headers const & headers)
-{
-    using namespace boost::python;
-
-    dict result;
-    for(auto const & item: headers)
-    {
-        result[item.first] = item.second;
-    }
-
-    return result;
-}
-
-odil::webservices::Message::Headers
-convert_headers(boost::python::dict const & headers)
-{
-    using namespace boost::python;
-    using namespace odil::webservices;
-
-    Message::Headers result;
-
-    auto items = headers.items();
-    for(int item_index=0; item_index<len(items); ++item_index)
-    {
-        auto item = items[item_index];
-
-        auto key_python = item[0];
-        std::string const key_cpp = extract<std::string>(key_python);
-
-        auto value_python = item[1];
-        std::string const value_cpp = extract<std::string>(value_python);
-
-        result.insert(std::make_pair(key_cpp, value_cpp));
-    }
-
-    return result;
-}
 
 boost::shared_ptr<odil::webservices::Message>
 constructor(
@@ -84,7 +46,7 @@ void wrap_webservices_Message()
     using namespace boost::python;
     using namespace odil::webservices;
 
-    scope message_scope = class_<Message>("Message", no_init)
+    class_<Message>("Message", no_init)
         .def(
             "__init__",
             make_constructor(
