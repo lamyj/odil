37762f14471af12445461fb20eb022dcee25a104
Julien Lamy <lamy@unistra.fr>
2018-02-02 07:50:39 +0100

    Add Python-like open

--- wrappers/python/__init__.py
+++ wrappers/python/__init__.py
@@ -1,1 +1,12 @@
+import contextlib
+
 from ._odil import *
+
+_open = open
+
+@contextlib.contextmanager
+def open(path, mode="r"):
+    fd = _open(path, mode)
+    stream = iostream(fd)
+    yield stream
+    fd.close()
