91bbc60cf9ffcd87c9c1c7e8cb6d9cfd07d6e99f
Julien Lamy <lamy@unistra.fr>
2020-08-28 13:03:57 +0200

    Update travis

---
+++ .ci/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CI_TYPE=${CI_TYPE:?}
+RUNNER=${RUNNER:?}
+WORKSPACE=${WORKSPACE:?}
+
+if [ "${RUNNER}" = "docker" ]; then
+  IMAGE=${IMAGE:?}
+  CONTAINER=${IMAGE//:/_}
+  PRE_START="docker run -di -v ${WORKSPACE}:${WORKSPACE} --name ${CONTAINER:?} --rm ${IMAGE}"
+  RUNNER="docker exec -w ${WORKSPACE} -e WORKSPACE=${WORKSPACE} ${CONTAINER:?}"
+  POST_END="docker kill ${CONTAINER:?}"
+else
+  PRE_START=""
+  RUNNER=""
+  POST_END=""
+fi
+
+${PRE_START}
+
+${RUNNER} ./.ci/${CI_TYPE}/install \
+  && ${RUNNER} ./.ci/${CI_TYPE}/build \
+  && ${RUNNER} ./.ci/${CI_TYPE}/post_build
+
+${POST_END}
