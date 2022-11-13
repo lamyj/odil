b6673d3840281a52184b579425904cf1ae503162
Julien Lamy <lamy@unistra.fr>
2022-11-13 16:55:35 +0100

    Don't require bioconda

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -1,6 +1,6 @@
 import subprocess
 
 subprocess.check_call([
-    "conda", "install", "--yes", "-c", "conda-forge", "-c", "bioconda",
-    "boost", "cmake", "dcmtk=3.6.7", "icu", "jsoncpp", "libssh2", "pkg-config",
+    "conda", "install", "--yes", "-c", "conda-forge",
+    "boost", "cmake", "dcmtk", "icu", "jsoncpp", "libssh2", "pkg-config",
     "pybind11"])
