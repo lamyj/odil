2d806c4f888213d527130bfe2df0b8ede211d309
Julien Lamy <lamy@unistra.fr>
2022-11-13 17:38:12 +0100

    Build with Ninja

--- .ci/build/apt.py
+++ .ci/build/apt.py
@@ -4,7 +4,7 @@ import subprocess
 os.environ["DEBIAN_FRONTEND"] = "noninteractive"
 subprocess.check_call([
     "apt-get", "-y", "--no-install-recommends", "install",
-    "cmake", "pkg-config", "g++", "make", "python3", "python3-requests",
+    "cmake", "pkg-config", "g++", "ninja-build", "python3", "python3-requests",
     "libboost-dev", "libboost-date-time-dev", "libboost-exception-dev",
     "libboost-log-dev", "libboost-filesystem-dev", "libboost-regex-dev",
     "libdcmtk2-dev", "libicu-dev", "libjsoncpp-dev", "zlib1g-dev",
--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -2,5 +2,5 @@ import subprocess
 
 subprocess.check_call([
     "conda", "install", "--yes", "-c", "conda-forge",
-    "boost", "cmake", "dcmtk", "icu", "jsoncpp", "libssh2", "pkg-config",
-    "pybind11"])
+    "boost", "cmake", "dcmtk", "icu", "jsoncpp", "libssh2", "ninja",
+    "pkg-config", "pybind11"])
